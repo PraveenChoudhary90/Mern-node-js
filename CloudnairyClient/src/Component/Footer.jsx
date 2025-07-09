7bd00f0c0a52cba0366ce68fd76d5d607ebd9207
praveen choudhary <pc852180@gmail.com>
2025-07-09 15:57:42 +0530

    Cloundnairy

---
+++ CloudnairyClient/src/Component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <h1 style={{padding:"40px", backgroundColor:"lightpink", textAlign:"center", fontWeight:"bold"}}>www.cloudnairy.com</h1>
+    </>
+  )
+}
+
+export default Footer
